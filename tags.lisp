549c518e0d3a27dc3486d84737082c92e92aac06
Bibek Panthi <bpanthi977@gmail.com>
2021-05-22 20:35:24 +0545

    changes are saved to .tags

--- tags.lisp
+++ tags.lisp
@@ -5,6 +5,7 @@
 (defparameter *$tag-directory* nil)
 (defparameter *.tag-file* nil)
 (defparameter *entries* nil)
+(defparameter *dirty-entries* nil)
 
 ;;;
 ;;; Data Structure 
@@ -19,6 +20,14 @@
   (tags nil :type (vector string))
   (%tags-string nil :type (or null string))
   (details nil :type details))
+
+(defun details= (d1 d2)
+  (= (details-size d1) (details-size d2)))
+
+(defun entry= (e1 e2)
+  (and (string= (entry-file e1) (entry-file e2))
+       (every #'string= (entry-tags e1) (entry-tags e2))
+       (details= (entry-details e1) (entry-details e2))))
 
 (defun tags-string (tags)
   "Converts vector of tags to tag-string"
@@ -55,8 +64,21 @@
 	  for tags = (coerce tags-list '(vector string))
 	  collect (make-entry
 		   :file file
-		   :tags (tags tags-string)
+		   :tags (make-array (length tags-list) :element-type 'string
+				     :initial-contents tags-list)
 		   :details (make-details :size (car details))))))
+
+(defun write-entries (entries .tag-file)
+  (with-open-file (stream .tag-file
+			  :direction :output
+			  :if-exists :supersede
+			  :if-does-not-exist :create)
+    (loop for entry in entries do
+      (write (list (entry-file entry)
+		   (coerce (entry-tags entry) 'list)
+		   (list (details-size (entry-details entry))))
+	     :stream stream))))
+  
 
 (defun create-entry (file tags)
   (make-entry
@@ -75,11 +97,29 @@
 ;;; Tagging System
 ;;;
 
+(defun save-changes ()
+  (when *dirty-entries*
+    (write-entries *entries* *.tag-file*)))
+
 (defmacro with-$tag-directory (($tag-directory &key (read-tags nil)) &body body)
   `(let* ((*$tag-directory* ,$tag-directory)
 	  (*.tag-file* (.tag-file *$tag-directory*))
-	  (*entries* (if ,read-tags (read-entries *.tag-file*) *entries*)))
-     ,@body))
+	  (*entries* (if ,read-tags (read-entries *.tag-file*) *entries*))
+	  (*dirty-entries* (progn (when *dirty-entries* 
+					(print "Dirty entries exist: ")
+					(print *dirty-entries*))
+				  ())))
+     (unwind-protect (progn ,@body)
+       (save-changes))))
+
+(defun push-change-in-entry (entry)
+  (let ((e (find (entry-file entry) *entries* :test #'string-equal :key #'entry-file)))
+    (cond ((not e)
+	   (push entry *entries*)
+	   (push entry *dirty-entries*))
+	  ((not (entry= entry e))
+	   (setf (nth (position e *entries*) *entries*) entry)
+	   (push entry *dirty-entries*)))))
 
 (defun tags->directories (tags)
   (declare (type (vector string) tags))
@@ -114,8 +154,13 @@
       (unless (= (length additions) 0)
 	(a:map-permutations (lambda (tags)
 			      (declare (type (vector string) tags))
-			      (create-link (link-pathname file (tags->directories tags)) file))
-			    (entry-tags new))))))
+			      (let ((path (link-pathname file (tags->directories tags))))
+				(unless (probe-file path) ; TODO: check if it points to same target
+				  (create-link path file))))
+			    (entry-tags new)))
+      (when (or (not (= (length deletions) 0))
+		(not (= (length additions) 0)))
+	(push-change-in-entry new)))))
 
 
 (defun existing-tags* (file)
